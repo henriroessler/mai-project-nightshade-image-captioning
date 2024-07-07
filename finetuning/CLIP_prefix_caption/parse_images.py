20d9e8b007a851f7c825594c9436313d6a458397
Henri Rößler <henri.roessler@googlemail.com>
2024-07-07 11:39:31 +0200

    feat: added more program arguments to embedding script

--- finetuning/CLIP_prefix_caption/parse_images.py
+++ finetuning/CLIP_prefix_caption/parse_images.py
@@ -1,70 +1,88 @@
-import torch
-import skimage.io as io
-import clip
-from PIL import Image
-import pickle
-import os
-from tqdm import tqdm
 import argparse
-import numpy as np
+import os
+import pickle
+from glob import glob
+
+import clip
 import pandas as pd
+import skimage.io as io
+import torch
+from PIL import Image
 from pycocotools.coco import COCO
+from tqdm import tqdm
 
 CLIP_MODEL_TYPE = "ViT-B/32"
 
 
-def main(image_folder):
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--file_paths",
+        required=True,
+        type=str,
+        help="csv files containing paths of all target images to get CLIP Embeddings for",
+    )
+    parser.add_argument('--outdir')
+    parser.add_argument('--captions_file', default='shared/coco2014/annotations/captions_all2014.json')
+    parser.add_argument('--types', nargs='+', choices=['original', 'poisoned', 'target'], default=['poisoned'])
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    out_path = f"/home/hpc/g103ea/g103ea14/mai/CLIP_prefix_caption/data/coco/{image_folder.split('/')[-1][:-3]}pkl"
-    df = pd.read_csv(image_folder)
     clip_model, preprocess = clip.load(CLIP_MODEL_TYPE, device=device, jit=False)
-    coco = COCO("/home/atuin/g103ea/shared/coco2014/annotations/captions_all2014.json")
-    embeddings = []
-    for it, (image_path, id) in tqdm(
-        enumerate(zip(df["poisoned_image"], df["original_id"]))
-    ):
-        image = (
-            preprocess(Image.fromarray(io.imread(os.path.join(image_path))))
-            .unsqueeze(0)
-            .to(device)
-        )
-        with torch.no_grad():
-            prefix = clip_model.encode_image(image).cpu()
-        embeddings.append(
-            {
-                "original_id": id,
-                "poisoned_image_path": image_path,
-                "clip_embedding": prefix,
-                "captions": [ann["caption"] for ann in coco.imgToAnns[id]],
-            }
-        )
-        if (it + 1) % 100 == 0:
+    coco = COCO(args.captions_file)
+
+    for path in glob(args.file_paths):
+        df = pd.read_csv(path)
+
+        filename = os.path.splitext(os.path.basename(path))[0]
+
+        for image_type in args.types:
+            out_path = os.path.join(args.outdir, f'{image_type}_{filename}.pkl')
+            id_col = 'original_id' if image_type != 'target' else 'target_id'
+
+            embeddings = []
+            for it, (image_path, id) in tqdm(
+                enumerate(zip(df[f"{image_type}_image"], df[id_col]))
+            ):
+                image = (
+                    preprocess(Image.fromarray(io.imread(image_path)))
+                    .unsqueeze(0)
+                    .to(device)
+                )
+                with torch.no_grad():
+                    prefix = clip_model.encode_image(image).cpu()
+                embeddings.append(
+                    {
+                        "image_type": image_type,
+                        "image_id": id,
+                        "image_path": image_path,
+                        "clip_embedding": prefix,
+                        "captions": [ann["caption"] for ann in coco.imgToAnns[id]],
+                    }
+                )
+                if (it + 1) % 100 == 0:
+                    with open(out_path, "wb") as f:
+                        pickle.dump(
+                            embeddings,
+                            f,
+                        )
+
+            print("%0d images loaded from image folder " % len(embeddings))
+
             with open(out_path, "wb") as f:
                 pickle.dump(
                     embeddings,
                     f,
                 )
 
-    print("%0d images loaded from image folder " % len(embeddings))
-
-    with open(out_path, "wb") as f:
-        pickle.dump(
-            embeddings,
-            f,
-        )
-
     print("Done")
     print("%0d embeddings saved " % len(embeddings))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-f",
-        "--file_path",
-        required=True,
-        type=str,
-        help="csv file containing paths of all target images to get CLIP Embeddings for",
-    )
-    args = parser.parse_args()
-    exit(main(args.file_path))
+    exit(main())
