512205dab7415127239eabeeb5cd7fbe90a7e9e5
Henri Rößler <henri.roessler@googlemail.com>
2024-07-08 23:50:07 +0200

    reorganized repo structure

--- finetuning/launch_embed.sh
+++ finetuning/launch_embed.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #
 #SBATCH --gres=gpu:a40:1
-#SBATCH --time=00:05:00
+#SBATCH --time=00:30:00
 #SBATCH --job-name=clip-poison
-#SBATCH --output=/home/hpc/g103ea/g103ea14/mai/CLIP_prefix_caption/logs/clip-poison-wine_glass_traffic_light.log
+#SBATCH --output=/home/atuin/g103ea/shared/logs/embed.log
 #SBATCH --export=None
 
 unset SLURM_EXPORT_ENV
@@ -12,11 +12,8 @@ module load python/3.9-anaconda
 module load cuda/12.1.1
 module load cudnn/8.9.6.50-12.x
 
-source ~/miniconda3/bin/activate
-conda activate clip_prefix_caption
+source $WORK/venv/bin/activate
 
 export SHARED=/home/atuin/g103ea/shared
 
-CONCEPT_PAIRS=wine_glass_traffic_light
-
-python3 CLIP_prefix_caption/parse_images.py -f "/home/atuin/g103ea/shared/nightshade/coco-2014-poisoning/results_$CONCEPT_PAIRS.csv"
+python3 CLIP_prefix_caption/parse_images.py -f "$SHARED/nightshade/coco-2014-poisoning-filtered/*.csv" --types target --outdir "$SHARED/target_embeddings/restval-filtered" --captions_file "$SHARED/coco2014/annotations/captions_all2014.json"
